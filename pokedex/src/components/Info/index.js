14de45b0581e83cfbeea8887ef8811f881425fc1
CENOS92 <beom5037@gmail.com>
2018-09-10 10:48:33 +0900

    Practice styled-components and PropTypes

--- pokedex/src/components/Info/index.js
+++ pokedex/src/components/Info/index.js
@@ -1,18 +1,54 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+import Proptypes from 'prop-types'
 
 class Info extends Component {
+  
+  componentDidUpdate (prevProps) {
+    // UX (로딩이 끝났다 등)
+    if (prevProps.pokemon !== this.props.pokemon){
+      // alert('업데이트 되었습니다.')
+    }
+  }
+  
   render() {
+    const { pokemon } = this.props;
+    const { id, name, weight, sprites } = pokemon;
+    // console.log(pokemon); // 처음 렌더링 땐 {}, setState로 update 되었을 땐 json 정보가 찍힘
+    
+    let images;
+    if (sprites) {
+      images = Object.keys(sprites).map((key) => {
+        const value = sprites[key];
+        return <img src={value} key={key} />
+      });
+    }
+
     return (
-      <div>
-        <img src=""/>
+      <Wrapper>
+        {images}
         <div>
-          <div>ID: 1</div>
-          <div>이름: 피카츄</div>
-          <div>무게 : 100</div>
+          <div>ID: {id}</div>
+          <div>이름: {name}</div>
+          <div>무게 : {weight}</div>
         </div>
-      </div>
+      </Wrapper>
     );
   }
 }
+
+/**
+ * styled의 장점: props를 넘길 수 있다
+ * JSX : <Wrapper isActive={isActive}>
+ * 
+ */
+const Wrapper = styled.div`
+  background-color: grey;
+`;
+
+Info.proptypes = {
+  pokemon: Proptypes.string
+}
+
 
 export default Info;
--- pokedex/src/components/Main/index.js
+++ pokedex/src/components/Main/index.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import Info from '../Info';
 
 /**
- * Life Cycle : 
+ * Life Cycle : 컴포넌트의 porps 또는 state가 변할 때 작동, 
+ * 각 시점별 로직을 작성하기 위함
+ * 
+ *  - componentDidMount: DOM에 그리고 난 직후, 
+ *    (statefull한 상황, e.g. kakao talk)여기서 Server를 계속 바라보고 있는 Listner를 설정
+ *  - componentDidUpdate: setState로 업데이트 된 순간
+ *  - componentWillUnmount: (라우팅 할 때) 이전 페이지가 사라지기 전에, 주로 Listener 해제하기 위해
  */
 
 class Main extends Component {
@@ -14,31 +21,78 @@ class Main extends Component {
     }
   }
 
+  // componentDidMount () {
+  //   console.log('componentDidMount: Rendered')
+  //   fetch('https://pokeapi.co/api/v2/pokemon/1/')
+  //   .then((response) => {
+  //     console.log('response', response);
+  //     return response.json()
+  //   })
+  //   .then((data) => {
+  //     const { id, name, weight, sprites } = data;
+  //     console.log({
+  //       id, 
+  //       name, 
+  //       weight, 
+  //       sprites
+  //     });
+  //   })
+  // }
+
+  async componentDidMount () {
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon/1/')
+    const data = await response.json(); // json으로 만드는게 왜 async로 되어야 하는지?
+    this.setState({
+      pokemon: data
+    })
+  }
+
+
   handleValue = (e) => {
     const {value} = e.target;
     this.setState({ value });
   }
 
-  handleClick = () => {
+  // handleClick = () => {
+  //   const { value } = this.state;
+  //   alert(`${value}가 입력되었습니다..`)
+  // }
+
+  handleClick = async () => {
     const { value } = this.state;
-    alert(`${value}가 입력되었습니다..`)
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${value}`);
+    const data = await response.json();
+    this.setState({
+      pokemon: data
+    })
   }
 
   render() {
     const { pokemon, value } = this.state;
     return (
-      <div>
+      <Wrapper>
         <Info pokemon={pokemon} />
         <div>
-          <input 
+          <StyledInput 
             value={value}
             onChange={this.handleValue}/>
           <button
             onClick={this.handleClick}>조회</button>
         </div>
-      </div>
+      </Wrapper>
     );
   }
 }
+
+const Wrapper = styled.div`
+  width: 500px;
+  margin: 0 auto;
+`;
+
+const StyledInput = styled.input`
+  height: 20px;
+  width: 100px;
+  border: 1px solid #333;
+`;
 
 export default Main;
