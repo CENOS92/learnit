4374d269140f583c285c43420d36ed58c04861e4
CENOS92 <beom5037@gmail.com>
2018-09-16 13:57:57 +0900

    Install firebase and Add Landing, Board Components

--- nyan-forum/src/views/App/index.js
+++ nyan-forum/src/views/App/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
-
+/**
+ * container component (UI 뿐 아니라 로직을 가진 컴포넌트)는 views에
+ * UI만 가지고 있는 컴포넌트 components에
+ */
 class App extends React.Component {
   render() {
     return (
