400597744b52d17091b933c259dd2069528c0703
CENOS92 <beom5037@gmail.com>
2018-09-16 14:09:59 +0900

    Route components with Switch

--- nyan-forum/src/views/App/index.js
+++ nyan-forum/src/views/App/index.js
@@ -1,4 +1,13 @@
 import React from 'react';
+import {
+  BrowserRouter, // Routing 최상단에 있는 컴포넌트 (앱의 최 상단)
+  Switch,
+  Route,
+} from 'react-router-dom';
+
+import Landing from '../Landing';
+import Board from '../Board';
+
 /**
  * container component (UI 뿐 아니라 로직을 가진 컴포넌트)는 views에
  * UI만 가지고 있는 컴포넌트 components에
@@ -6,9 +15,13 @@ import React from 'react';
 class App extends React.Component {
   render() {
     return (
-      <div>
-        App
-      </div>
+      <BrowserRouter>
+        <Switch>
+          {/* exact는 포함되었더라도 정확히 일치하는 것만! 가장 상위에는 exact를 사용해야 함 `exact={true}` */}
+          <Route path="/" exact component={Landing} />
+          <Route path="/board" component={Board} />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
