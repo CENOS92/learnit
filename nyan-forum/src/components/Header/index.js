454cfe3218e832b0889cbea34a6d95887af1acdf
CENOS92 <beom5037@gmail.com>
2018-10-02 01:29:49 +0900

    Fix minor bug: assign 'postId'

--- nyan-forum/src/components/Header/index.js
+++ nyan-forum/src/components/Header/index.js
@@ -9,7 +9,7 @@ class Header extends React.Component {
   render() {
     const { pathname } = this.props.location;
     console.log("Header pathname", pathname)
-    const { postId } = pathname.split('/')[3];
+    const postId = pathname.split('/')[3];
     const { isCreate } = pathname.includes('/create');
     
     return (
@@ -29,4 +29,15 @@ class Header extends React.Component {
   }
 }
 
-export default withRouter(Header);
+const Wrapper = styled.div`
+  max-width: 1004px;
+  margin: 0 auto;
+  padding: 5rem 0;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+
+export default withRouter(Header); // withRouter 없으면 pathname을 가지고 올 수 없음
