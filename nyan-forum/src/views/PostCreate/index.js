9105d5fb91e0efa6c5a19aaac1afbfc2cfd5aa0e
CENOS92 <beom5037@gmail.com>
2018-10-02 01:40:34 +0900

    Implement base elements of PostCreate

--- nyan-forum/src/views/PostCreate/index.js
+++ nyan-forum/src/views/PostCreate/index.js
@@ -2,20 +2,42 @@ import React from 'react';
 import { db } from '../../firebase'
 
 class PostCreate extends React.Component {
-  
+  state = {
+    author: '',
+    title: '',
+    content: ''
+  };
+
+
   async componentDidMount() {
     const { boardId } = this.props.match.params;
     const docSnapshpt = await db.collection('boards').doc(boardId).get();
     const board  = docSnapshpt.data();
     console.log(board)
-
   }
 
   render() {
     const { boardId } = this.props.match.params;
     console.log("boardId", boardId)
+    const { author, title, content } = this.state;
     return (
-      <div>PostCreate</div>
+      <div>
+        <form action="">
+          <div>
+            <label>닉네임</label>
+            <input value={author} onChange={this.onChange} />
+          </div>
+          <div>
+            <label>제목</label>
+            <input value={title} onChange={this.onChange} />
+          </div>
+          <div>
+            <label>내용</label>
+            <input value={content} onChange={this.onChange} />
+          </div>
+          <button>글 쓰기</button>
+        </form>
+      </div>
     );
   }
 }
