3ffbfc6900d06a796be41dad22e9de57a1ab0269
CENOS92 <beom5037@gmail.com>
2018-10-02 14:17:22 +0900

    Apply styles

--- nyan-forum/src/views/PostCreate/index.js
+++ nyan-forum/src/views/PostCreate/index.js
@@ -54,25 +54,86 @@ class PostCreate extends React.Component {
     console.log("boardId", boardId)
     const { author, title, content } = this.state;
     return (
-      <div>
-        <form action="">
-          <div>
-            <label>닉네임</label>
-            <input value={author} onChange={this.handleChange('author')} />
-          </div>
-          <div>
-            <label>제목</label>
-            <input value={title} onChange={this.handleChange('title')} />
-          </div>
-          <div>
-            <label>내용</label>
-            <input value={content} onChange={this.handleChange('content')} />
-          </div>
+      <Wrapper>
+        <Form action="">
+          <Nickname>
+            <Label>닉네임</Label>
+            <Input value={author} onChange={this.handleChange('author')} />
+          </Nickname>
+          <Title>
+            <Label>제목</Label>
+            <Input value={title} onChange={this.handleChange('title')} />
+          </Title>
+          <Content>
+            <Label>내용</Label>
+            <Textarea value={content} onChange={this.handleChange('content')} />
+          </Content>
           <button onClick={this.handleSubmit}>글 쓰기</button>
-        </form>
-      </div>
+        </Form>
+      </Wrapper>
     );
   }
 }
+
+
+const Wrapper = styled.div`
+  width: 100%;
+  padding: 5rem 4rem;
+  border: 1px solid #eee;
+  border-radius: 3px;
+  background-color: #fff;
+`;
+
+const Form = styled.form`
+  text-align: center;
+`;
+
+const Nickname = styled.div`
+  margin-bottom: 4rem;
+`;
+
+const Title = styled.div`
+  margin-bottom: 4rem;
+`;
+
+const Content = styled.div`
+  margin-bottom: 4rem;
+`;
+
+const Label = styled.div`
+  text-align: left;
+  font-size: 1.8rem;
+  font-weight: 700;
+  color: #333;
+  margin-bottom: 2rem;
+`;
+
+const Input = styled.input`
+  font-family: 'NanumSquare';
+  box-sizing : border-box;
+  height: 4rem;
+  width: 100%;
+  background-color: #f9f9f9;
+  border: 1px solid #eee;
+  border-radius: 3px;
+  padding: 0rem;
+  padding-left: 1rem;
+  font-size: 1.6rem;
+  color: #333;
+`;
+
+const Textarea = styled.textarea`
+  resize: none;
+  font-family: 'NanumSquare';
+  box-sizing : border-box;
+  width: 100%;
+  height: 15rem;
+  background-color: #f9f9f9;
+  border: 1px solid #eee;
+  border-radius: 3px;
+  font-size: 1.6rem;
+  color: #333;
+  padding: 1rem;
+`;
 
 export default PostCreate;
