1879bd0962f0cd0f489694cb38a36a2ee2c847a9
CENOS92 <beom5037@gmail.com>
2018-10-02 16:12:49 +0900

    Update firebase db on submit

--- nyan-forum/src/views/CommentCreate/index.js
+++ nyan-forum/src/views/CommentCreate/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import { db } from '../../firebase';
 
 import Button from '../../components/Button'
 
@@ -18,6 +19,32 @@ class CommentCreate extends Component {
   handleSubmit = async (evt) => {
     evt.preventDefault();
     const confirm = window.confirm('정말로 등록하시겠습니까?');
+
+    if (confirm) {
+      const { postId } = this.props.match.params;
+      const { author, content } = this.state;
+
+      // 새로운 댓글을 Comments Collection에 추가
+      const newCommentRef = db.collection('comments').doc();
+      await newCommentRef.set({
+        id: newCommentRef.id,
+        author,
+        content
+      });
+
+      // 새로 생성된 코멘트의 Id를 Post Collection의 comments에 추가
+      const postSnapshot = await db.collection('posts').doc(postId).get();
+      const commentsByPost = postSnapshot.data().comments;
+      await db.collection('posts').doc(postId).update({
+        comments: [
+          ...commentsByPost,
+          newCommentRef.id
+        ]
+      })
+
+      alert('성공적으로 등록되었습니다.');
+      this.props.history.goBack();
+    }
   }
   
   render() {
